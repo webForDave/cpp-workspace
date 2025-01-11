fb466f46285ff8faac832732aed070e9f73b6689
David Akinola <dakinola54@gmail.com>
2025-01-11 17:32:27 +0100

    multiply two numbers from stream

---
+++ sample.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+
+int main()
+{
+    int num1, num2;
+    std::cout << "Enter two numbers " << std::endl;
+    std::cin >> num1 >> num2;
+    std::cout << "The product of " << num1 << "and " << num2 << " is " << num1 * num2;
+
+    return 0;
+}
