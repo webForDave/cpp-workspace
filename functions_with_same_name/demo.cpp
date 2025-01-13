f2095fd3939401980d21475e209010cf4ae6e515
David Akinola <dakinola54@gmail.com>
2025-01-13 07:27:28 +0100

    first overload

---
+++ functions_with_same_name/demo.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+void user1();
+int main()
+{
+    user1();
+    return 0;
+}
+
+
+void print(int x){
+    cout << x << endl;
+}
+
+void print(double x){
+    cout << x << endl;
+}
+
+void print(string x){
+    cout << x << endl;
+}
+
+
+void user1(){
+    print(5);
+}
