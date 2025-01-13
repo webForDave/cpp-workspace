7f393614b75e581cb77e5a21131799f42332e689
David Akinola <dakinola54@gmail.com>
2025-01-13 07:33:45 +0100

    fourth overload

--- functions_with_same_name/demo.cpp
+++ functions_with_same_name/demo.cpp
@@ -2,9 +2,11 @@
 using namespace std;
 
 void user1();
+void user2();
 int main()
 {
     user1();
+    user2();
     return 0;
 }
 
@@ -21,9 +23,17 @@ void print(string x){
     cout << x << endl;
 }
 
+void print(string x, int y){
+    cout << "Arg 1: " << x << endl << "Arg 2: " << y << endl;
+}
+
 
 void user1(){
-    print(5);
-    print(2.5);
-    print("Hello world");
+    //print(5);
+    //print(2.5);
+    //print("Hello world");
+}
+
+void user2(){
+    print("Hello world", 5);
 }
