67268d6a93179d04a0753428b895ebf4425056d2
David Akinola <dakinola54@gmail.com>
2025-02-10 17:22:15 +0100

    initial commit for character check cpp program

---
+++ char_check.cpp
@@ -0,0 +1,15 @@
+/**
+ * Character check - checks weather a character is a vowel or a consonant
+ * This program takes one chracter from a user,
+ * and prints to the console what sound it is either Vowel or Consonant
+ */
+
+ #include <iostream>
+ char user_char; // variable to store the character inputted by user
+
+ int main()
+ {
+    /**
+     * TODO: get back to implement this
+     */
+ }
