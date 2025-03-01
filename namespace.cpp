88823bb17c9888128aec69b9528fc4b0f5feb3b6
David Akinola <dakinola54@gmail.com>
2025-03-01 20:08:52 +0100

    removed namespace std from global scope

--- namespace.cpp
+++ namespace.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
-using namespace std;
+void new_function();
 
 int myInt;
 
 int main()
 {
+    using namespace std;
     cout << "Enter favorite number " << endl;
     cin >> myInt;
     cout << "You entered " << myInt << " added with 100 is " << myInt + 100 << endl;
+    new_function();
     return 0;
+}
+
+void new_function()
+{
+    cout << "Hello from function beneath main" << endl;
 }
