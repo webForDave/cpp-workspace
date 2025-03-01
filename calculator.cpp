8fb42cf649382d4469737ffe47698c36659d0979
David Akinola <dakinola54@gmail.com>
2025-03-01 20:36:59 +0100

    added subtraction namespace

--- calculator.cpp
+++ calculator.cpp
@@ -7,10 +7,28 @@ namespace addition{
     }
 }
 
+namespace subtraction{
+    int sub(int a, int b){
+        return a - b;
+    }
+}
+
+char operand;
 int a,b;
 
 
 int main()
 {
+    cout << "Enter an operand ";
+    cin >> operand;
+    cout << "Enter two numbers ";
+    cin >> a >> b;
+    
+    if (operand == '+'){
+        cout << addition::add(a, b);
+    }
+    else if(operand == '-'){
+        cout << subtraction::sub(a, b);
+    }
     return 0;
 }
