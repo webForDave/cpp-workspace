41d6cb53a7aa65f6b0a835c31b49651e39a26a4e
David Akinola <dakinola54@gmail.com>
2025-03-01 20:03:57 +0100

    Standard namespace

---
+++ namespace.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int myInt;
+
+int main()
+{
+    cout << "Enter favorite number " << endl;
+    cin >> myInt;
+    cout << "You entered " << myInt << " added with 100 is " << myInt + 100 << endl;
+    return 0;
+}
