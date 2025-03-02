d58695135fb9cfc2b58f08799a5faee1b2e9b90c
David Akinola <dakinola54@gmail.com>
2025-03-02 17:04:44 +0100

    moved first identifier out of first namespace

--- nsp.cpp
+++ nsp.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 using namespace std;
 
+int first_variable = 10;
+
 // creating a namespace with multiple children
 namespace myNamespace{
-    int first_variable = 10;
     namespace secondNamespace{
         void logMessage(){
             cout << first_variable << endl;
         }
         namespace thirdNamspace{
+            int second_var = 20;
             void thirdMessage(){
                 cout << "Hello from third namespace" << endl;
             }
